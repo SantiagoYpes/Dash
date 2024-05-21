785c455a679c1cae8ecc93374f4402f8918792d0
SantiagoYpes <santiyezu8@hotmail.com>
2024-05-20 21:08:59 -0500

    .

--- app.py
+++ app.py
@@ -4,11 +4,12 @@ import pandas as pd
 import plotly.express as px
 
 # Incorporate data
-df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/gapminder2007.csv')
-df1 = pd.read_csv('https://docs.google.com/spreadsheets/d/e/2PACX-1vR39zCK50jRbeuJonUUGCWGa5t1psOH98nuZrZpZtVUtS8j_EFGg2WwqlTZmSlkjmGI6wK_HIIqKsR3/pub?gid=789094753&single=true&output=csv')
-df1['order_value_EUR'] = df1['order_value_EUR'].str.replace(',', '').astype(float)
-df1['date'] = pd.to_datetime(df1['date'], format='%m/%d/%Y')
-print(df1.info())
+df1 = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/gapminder2007.csv')
+df = pd.read_csv('https://docs.google.com/spreadsheets/d/e/2PACX-1vR39zCK50jRbeuJonUUGCWGa5t1psOH98nuZrZpZtVUtS8j_EFGg2WwqlTZmSlkjmGI6wK_HIIqKsR3/pub?gid=789094753&single=true&output=csv')
+df['order_value_EUR'] = df['order_value_EUR'].str.replace(',', '').astype(float)
+df['date'] = pd.to_datetime(df['date'], format='%m/%d/%Y')
+df['profit']= df['order_value_EUR']-df['cost']
+
 
 # Initialize the app
 app = Dash(__name__)
@@ -25,22 +26,7 @@ app.layout = html.Div([
             ),
     html.Div([
         
-        html.Div([
-            dcc.RadioItems(
-                options=[
-                    {'label': 'Population', 'value': 'pop'},
-                    {'label': 'Life Expectancy', 'value': 'lifeExp'},
-                    {'label': 'GDP per Capita', 'value': 'gdpPercap'}
-                ],
-                value='lifeExp',
-                id='controls-and-radio-item'
-            ),
-            dcc.Graph(
-                figure={},
-                id='controls-and-graph'
-            )
-            
-        ], style={'width': '48%', 'display': 'inline-block', 'vertical-align': 'top'}),
+        
         
         html.Div([
             dcc.RadioItems(
@@ -65,7 +51,7 @@ app.layout = html.Div([
     Input(component_id='controls-and-radio-item', component_property='value')
 )
 def update_graph(col_chosen):
-    fig = px.histogram(df, x='continent', y=col_chosen, histfunc='avg')
+    fig = px.histogram(df, x='profit', y='category', histfunc='avg')
     return fig
 
 # Run the app
